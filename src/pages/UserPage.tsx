175490f8736f2c1a84f4b294ad33dabbb3bcd877
mymiracle0118 <noahsflood908@gmail.com>
2024-10-01 14:32:21 +0900

    Add User Page

---
+++ src/pages/UserPage.tsx
@@ -0,0 +1,14 @@
+// UserPage.tsx
+import React from 'react';
+import User from '../components/User';
+
+const UserPage: React.FC = () => {
+    return (
+        <div>
+            <h1>User Page</h1>
+            <User />
+        </div>
+    );
+};
+
+export default UserPage;
